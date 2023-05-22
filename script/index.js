60c13993cdc6870ff78c745f01a84ee99707f3e7
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-05-22 16:26:42 +0200

    Almost finished

--- script/index.js
+++ script/index.js
@@ -1,11 +1,46 @@
 let display = document.getElementById("screen");
+let clear = document.getElementById("clear");
+let del = document.getElementById("del");
+
 
 function calculation(number) {
-    display.value += number;
-};
+    //  display.value += number;
+     if (number === '.' && display.value.includes('.')) {
+         return false
+     }
+     else if(number !== '.' && display.value) {
+        return true;
+     }
+
+     display.value += number;
+
+    // let output = input.slice(-1)
+    // let operators = ["+", "-", "*", "/"]
+
+    // if (number == "." && output == ".") {
+    //     return false
+    // }
+    // if (operators.includes(number)) {
+    //     if (operators.includes(output)) {
+    //         return false
+    //     }
+
+    //     else {
+    //         return true
+    //     }
+    // }
+    // display.value += number;
+}
 
 
 function answer() {
     display.value = eval(display.value)
 }
 
+clear.addEventListener('click', function clear() {
+    display.value = "";
+});
+
+del.addEventListener('click', function () {
+    display.value = display.value.slice(0, -1);
+});
